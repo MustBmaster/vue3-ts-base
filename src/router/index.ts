4d9386a3081bd198cd3526d28c881fdae26f5f6d
minhnv1 <99402972+MustBmaster@users.noreply.github.com>
2025-01-09 15:43:04 +0700

    config #1

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import Layout from '@/layout'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -12,7 +12,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      component: Layout,
     },
   ],
 })

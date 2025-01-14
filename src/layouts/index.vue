91df4e4c7d6563f062cf25190605b898488519b5
minhnv1 <99402972+MustBmaster@users.noreply.github.com>
2025-01-14 13:43:14 +0700

    page frame created

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -1,7 +1,153 @@
 <template>
-  <div class="minhnv1">minhn1minhn1</div>
+  <div class="page-container d-flex">
+    <SideNav />
+    <div class="w-100">
+      <div class="page-header">
+        <h4 class="m-0">pageherdeer</h4>
+      </div>
+      <div class="content-frame">
+        <el-scrollbar max-height="100%">
+          <div class="bg-white page-content">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis consequuntur
+            voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis deserunt
+            impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem veritatis
+            consequuntur voluptates illo tempore quas sunt optio eligendi. Suscipit optio quod omnis
+            deserunt impedit doloribus quae, nobis eaque obcaecati. Ratione. Lorem, ipsum dolor sit
+            amet consectetur adipisicing elit. Dignissimos, dolorem facere? Soluta accusantium saepe
+            alias praesentium mollitia dolores eaque vero, amet incidunt modi enim delectus? Libero
+            tempore velit quod debitis. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Quasi necessitatibus, delectus, dolorem enim dolores nostrum rerum esse, dolor quidem
+            maxime consectetur voluptatem harum soluta modi ipsa eaque commodi voluptate
+            repudiandae.
+          </div>
+        </el-scrollbar>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-// import TheWelcome from '../components/TheWelcome.vue'
+import SideNav from './Sidenav/index.vue'
 </script>
+<style lang="scss" scoped>
+.page-container {
+  height: 100vh;
+  width: 100vw;
+}
+.content-frame {
+  padding: 24px 0 24px 24px;
+  background-color: red;
+  height: calc(100vh - 72px);
+}
+
+.page-header {
+  height: 72px;
+  padding: 22px 24px;
+  background-color: white;
+}
+.page-content {
+  padding-right: 24px;
+}
+</style>
